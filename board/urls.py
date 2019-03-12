d9ac41313a01cdd57b5ade80611b2e50ea60812a
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-03-12 16:18:22 +0300

    define app urls

--- board/urls.py
+++ board/urls.py
@@ -1,23 +1,31 @@
 from django.conf.urls import url
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
+
+from .views import ajax
+from .views import general
 
 app_name = 'board'
-urlpatterns=[
-    url('^$',views.timeline, name='timeline'),
-    url(r'^profile/', views.user_profile, name='user_profile'),
-    url(r'^upload/', views.upload_content, name='upload_content'),
-    url(r'^search/', views.search_user, name='search_user'),
-    url(r'^modal/', views.view_modal, name='view_modal'),
-    url(r'^followers/', views.followers, name='followers'),
-    url(r'^following/', views.following, name='following'),
-    url(r'^post/', views.post, name='post'),
-    ]
+urlpatterns = [
+    url(r'^$', general.index, name='index'),
+    url(r'^login/$', general.user_login, name='user_login'),
+    url(r'^logout/$', general.user_logout, name='user_logout'),
+    url(r'^upload/$', general.upload_photo, name='upload_photo'),
+    url(r'^users/$', general.users, name='users'),
+    url(r'^u/(?P<username>[\w-]+)$', general.user_profile, name='user_view'),
+    url(r'^profile/$', general.user_profile, name='user_profile'),
+    url(r'^following/$', general.user_following, name='user_following'),
+    url(r'^followers/$', general.user_followers, name='user_followers'),
+    url(r'^feed/$', general.feed, name='feed'),
+    url(r'^search/$', general.search, name='user_search'),
+
+    # AJAX methods
+    url(r'^upload_dp/$', ajax.upload_user_profile_pic,
+        name='upload_user_profile_pic'),
+    url(r'^follow/$', ajax.follow_user, name='follow_user'),
+    url(r'^unfollow/$', ajax.unfollow_user, name='unfollow_user'),
+    url(r'^post_comment/$', ajax.post_photo_comment, name='post_photo_comment'),
+    url(r'^like_comment/$', ajax.like_photo, name='like_photo'),
+]
+
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-
-
-
---
+++ board/views/ajax.py
@@ -0,0 +1,5 @@
+upload_user_profile_pic
+follow_user
+unfollow_user
+post_photo_comment
+ajax.like_photo
