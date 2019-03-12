528aafa04d437f65492f50a5757a8d352171f4ee
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-03-12 16:03:32 +0300

    define static files paths

--- archigram/settings.py
+++ archigram/settings.py
@@ -126,12 +126,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, "static")
 ]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/uploads/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-LOGIN_REDIRECT_URL = "profile"
+LOGIN_URL = '/insta/login/'
+
+
